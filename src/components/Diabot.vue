4a3e4813cc1689770fb35b89d0486fc806dcaee6
walters-skn <mickywalters72@gmail.com>
2023-06-25 10:48:06 -0500

    Updated

--- src/components/Diabot.vue
+++ src/components/Diabot.vue
@@ -1,7 +1,25 @@
+
 <template>
+    <div id="app">
+      <nav class="navbar navbar-expand navbar-light fixed-top">
+        <div class="container">
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a href="./src/components/HomePage.vue" class="nav-link"> <strong> Home || </strong></a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link"><strong> About Us </strong></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  <div class="chatbox-container"> 
     <div class="chatbox">
       <div class="chatbox-header">
-        <h3>Chat</h3>
+        <h2 class="head"> DIABOT CHAT </h2>
       </div>
       <div class="chatbox-body">
         <div v-for="(message, index) in messages" :key="index" class="message" :class="{ 'user-message': message.isUserMessage }">
@@ -12,79 +30,120 @@
       </div>
       <div class="chatbox-footer">
         <input type="text" v-model="newMessage" placeholder="Type a message..." @keydown.enter="sendMessage">
-        <button @click="sendMessage">Send</button>
+        <button class="btn" @click="sendMessage">Send</button>
       </div>
     </div>
+  </div>
   </template>
   
-  <script>
-  export default {
-    data() {
-      return {
-        messages: [],
-        newMessage: ''
-      };
-    },
-    methods: {
-      sendMessage() {
-        if (this.newMessage.trim() !== '') {
-          this.messages.push({ content: this.newMessage, isUserMessage: true });
-          this.newMessage = '';
+<script>
+    export default {
+        data() {
+        return {
+            messages: [],
+            newMessage: ''
+        };
+        },
+        methods: {
+        sendMessage() {
+            if (this.newMessage.trim() !== '') {
+            this.messages.push({ content: this.newMessage, isUserMessage: true });
+            this.newMessage = '';
+            }
         }
-      }
-    }
-  };
-  </script>
+        }
+    };
+</script>
   
-  <style scoped>
+<style scoped>
+
+  /* navigation bar */
+  .navbar {
+      padding: 0px;
+      background-color: #5ca2b1;
+    }
+  
+    /* navigation link */
+    .nav-link{
+      color: #000000;
+    }
+  
+    .collapse{
+      font-family: Georgia, 'Times New Roman', Times, serif;
+      font-size: 20px;
+      display: flex;
+      justify-content: flex-end;
+    }
+  
+    .chatbox-container{
+      height:100vh;
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
+    }
+    
+    .head{
+    font-family: fantasy;
+    color: white;  
+    text-align: center;
+    margin: 0;
+    font-size:45px;
+}
+
   .chatbox {
-    width: 300px;
-    height: 400px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
+    width: 100%;
+    max-width: 100%;
+    height: 100%;
     display: flex;
-    flex-direction: column;
+    flex-direction: row; /*this allows the "Diabot Chat" to go on the left side of the page*/
   }
   
+  
   .chatbox-header {
-    background-color: #f5f5f5;
-    padding: 10px;
-    text-align: center;
+    padding: 50px;
+    background-color: #5ca2b1;
+    display: flex;
+    align-items: center;
   }
   
   .chatbox-body {
     flex: 1;
-    padding: 10px;
     overflow-y: auto;
-  }
-  
-  .message {
-    padding: 5px;
-    margin-bottom: 5px;
-  }
-  
-  .user-message {
-    background-color: #f0f0f0;
-  }
-  
-  .message-content {
-    padding: 5px;
   }
   
   .chatbox-footer {
     display: flex;
-    align-items: center;
+    align-items: flex-end;
     padding: 10px;
+    width: 100%;
+    font-size: 14px;
   }
   
   .chatbox-footer input[type="text"] {
     flex: 1;
     margin-right: 10px;
-    padding: 5px;
+    padding: 7px;
+    font-size: 14px;
+    text-align: left;
   }
   
   .chatbox-footer button {
     padding: 5px 10px;
+    background-color: #5599ab;
+    color: white;
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 14px;
   }
+  
+  .message {
+    padding: 5px;
+  font-size: 15px;
+  }
+  
+  .message-content {
+    padding: 5px;
+    font-size: 16px;
+  }
+
   </style>
   
