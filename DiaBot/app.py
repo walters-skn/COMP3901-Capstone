4978c0efaf00cc30f749077702eef7a774df4636
e277 <ezramuir12@gmail.com>
2023-07-24 00:32:59 -0500

    Updated notification and diabot pages

--- DiaBot/app.py
+++ DiaBot/app.py
@@ -8,6 +8,7 @@ from db.connect import db_config
 app = create_app()
 app.config.from_object(__name__)
 app.config["JWT_SECRET_KEY"] = "comp3901-secret-key"
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = False
 jwt = JWTManager(app)
 
 CORS(app, resources={r'/*': {'origins': '*'}})
